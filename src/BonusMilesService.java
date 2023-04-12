d01c2bd59f0ee1e842502de23204e1ece77fe9fd
Alexander Kovalchuk <beckham3@yandex.ru>
2023-04-12 14:53:39 +0300

    add src

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate(int a) {
+        int cost;
+           if (a>=20) {
+               cost = a/20;
+       }else { cost=0;
+    }
+    return cost;
+    }
+}
