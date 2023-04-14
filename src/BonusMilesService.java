4ce91c460aec7203a78dfed475ddeeb3e6000069
Alexander Kovalchuk <beckham3@yandex.ru>
2023-04-14 18:42:12 +0300

    changed BonusMilesService.java and Main.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,12 @@
 public class BonusMilesService {
 
-    public int calculate(int a) {
+    public int calculate(int price) {
         int cost;
-           if (a>=20) {
-               cost = a/20;
-       }else { cost=0;
-    }
-    return cost;
+        if (price >= 20) {
+            cost = price / 20;
+        } else {
+            cost = 0;
+        }
+        return cost;
     }
 }
